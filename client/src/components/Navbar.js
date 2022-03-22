35b6d27702328735c7d71ff5270ce91f75765d76
MeKa <48958010+metinkantar@users.noreply.github.com>
2022-03-22 19:28:16 +0300

    Update Navbar.js

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -6,7 +6,7 @@ function Navbar() {
         <header>
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
                 <div className="container-fluid">
-                    <a className="navbar-brand fw-bold text-dark fs-3" href="/">KANTARSOFT</a>
+                    <a className="navbar-brand fw-bold text-dark fs-3" href="/">@MeKa Logo</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
